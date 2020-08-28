db8f578f43f51700c1be45c092c242e043a59e84
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-08-29 02:14:39 +0300

    Add

---
+++ src/getWeather.sh
@@ -0,0 +1,61 @@
+getDailyWeatherForecast () {
+  curl -H "Origin:https://mgm.gov.tr" https://servis.mgm.gov.tr/web/tahminler/saatlik\?istno\=17060
+}
+
+getTahminArray () {
+  cat "$1" |jq -r '.[] | .tahmin'
+}
+
+getHourCountFromTahmin () {
+  cat "$1" |jq -r 'length'
+}
+
+getWeatherStatusFromLetters () {
+  weatherStatus=$(case $1 in
+    A) echo Açık;;
+    AB) echo Az Bulutlu;;
+    PB) echo Parçalı Bulutlu;;
+    CB) echo Çok Bulutlu;;
+    HY) echo Hafif Yağmurlu;;
+    Y) echo Yağmurlu;;
+    KY) echo Kuvvetli Yağmurlu;;
+    KKY) echo Karla Karışık Yağmurlu;;
+    HKY) echo Hafif Kar Yağışlı;;
+    K) echo Kar Yağışlı;;
+    YKY) echo Yoğun Kar Yağışlı;;
+    HSY) echo Hafif Sağanak Yağışlı;;
+    SY) echo Sağanak Yağışlı;;
+    KSY) echo Kuvvetli Sağanak Yağışlı;;
+    MSY) echo Mevzi Sağanak Yağışlı;;
+    DY) echo Dolu;;
+    GSY) echo Gökgürültülü Sağanak Yağışlı;;
+    KGY) echo Kuvvetli Gökgürültülü Sağanak Yağışlı;;
+    SIS) echo Sisli;;
+    PUS) echo Puslu;;
+    DMN) echo Dumanlı;;
+    KF) echo Kum veya Toz Taşınımı;;
+    R) echo Rüzgarlı;;
+    GKR) echo Güneyli Kuvvetli Rüzgar;;
+    KKR) echo Kuzeyli Kuvvetli Rüzgar;;
+    SCK) echo Sıcak;;
+    SGK) echo Soğuk;;
+    HHY) echo Yağışlı;;
+    *) echo Bilinmiyor;;
+  esac)
+  echo $weatherStatus
+}
+
+getDailyWeatherForecast > forecast.json
+getTahminArray forecast.json > tahmin.json
+hourCount=$(getHourCountFromTahmin tahmin.json)
+tahmin=$(cat tahmin.json)
+for ((i=1; i<$hourCount; i++))
+do
+  read -A arr < <(echo $(echo $tahmin | jq -r --arg i $i '.[$i|tonumber] | .tarih, .hadise, .sicaklik'))
+  date=${arr[1]}
+  weatherLetters=${arr[2]}
+  temp=${arr[3]}
+  hour=$(gdate --date="$date" +%R)
+  weatherText=$(getWeatherStatusFromLetters $weatherLetters)
+  echo $hour $temp"ºC" $weatherText
+done
