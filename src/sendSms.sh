93a92231cebdd105ee59c89c741afc362f02252e
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-08-29 19:29:46 +0300

    Fix

--- src/sendSms.sh
+++ src/sendSms.sh
@@ -1,6 +1,6 @@
 BASE_URL='https://api.twilio.com'
 echo "::set-env name=SMS_TEXT::$(echo 'zaferayan')"
-curl -X POST ${BASE_URL}/2010-04-01/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Messages.json \
+curl -X POST ${BASE_URL}/2010-04-01/Accounts/$(echo process.env.TWILIO_ACCOUNT_SID)/Messages.json \
     --data-urlencode "Body=$(cat sms.txt)" \
     --data-urlencode "From=${process.env.MSISDN_SENDER}" \
     --data-urlencode "To=${process.env.MSISDN_RECEIVER}" \
